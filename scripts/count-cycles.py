4c8d8675992f6121fb4d0463dfefa00dde6e93b4
Ariel Szekely <arielck@mit.edu>
2023-05-17 10:17:20 -0400

    x

--- scripts/count-cycles.py
+++ scripts/count-cycles.py
@@ -2,18 +2,27 @@
 
 import argparse
 
-def count_cgroup_cycles(cgroup):
- with open("/sys/fs/cgroup/cpu,cpuacct/system.slice/{}/cpuacct.usage".format(cgroup), "r") as f: 
+def cgroup_path(cgroup):
+  return "/sys/fs/cgroup/cpu,cpuacct/system.slice/{}/cpuacct.usage".format(cgroup)
+
+def get_ns(p):
+ with open(p, "r") as f: 
    ns = int(str(f.read()).strip())
- print(ns)
+ return ns
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("--cgroup", type=str, required=True)
+  parser.add_argument("--prev", type=str, default=None)
 
   args = parser.parse_args()
 
-  count_cgroup_cycles(args.cgroup)
+  cnt = get_ns(cgroup_path(args.cgroup))
+  if args.prev is not None:
+    prev = get_ns(args.prev)
+    print("Nanoseconds difference: {}".format(cnt - prev))
+  else:
+    print("Nanoseconds: {}".format(cnt))
 
 if __name__ == "__main__":
   main()
