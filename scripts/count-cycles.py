a6c567de1d1d50019213f404c024b33b21e4c386
Ariel Szekely <arielck@mit.edu>
2023-05-17 10:10:34 -0400

    x

---
+++ scripts/count-cycles.py
@@ -0,0 +1,19 @@
+#!/bin/python3 
+
+import argparse
+
+def count_cgroup_cycles(cgroup):
+ with open("/sys/fs/cgroup/cpu,cpuacct/system.slice/{}/cpuacct.usage".format(cgroup), "r") as f: 
+   ns = int(str(f.read()).trim())
+ print(ns)
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--cgroup", type=str, required=True)
+
+  args = parser.parse_args()
+
+  count_cgroup_cycles(args.cgroup)
+
+if __name__ == "__main__":
+  main()
