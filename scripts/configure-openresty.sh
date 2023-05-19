e2c1f0afb5ccb453f7845f3821b6e5418740aa81
Ariel Szekely <arielck@mit.edu>
2023-05-18 21:00:42 -0400

    x

--- scripts/configure-openresty.sh
+++ scripts/configure-openresty.sh
@@ -4,3 +4,5 @@ DIR=$(dirname $0)
 
 sudo cp $DIR/../websrv/openresty/nginx.conf /etc/openresty/nginx.conf
 sudo systemctl restart openresty
+
+echo "4096" | sudo tee /sys/fs/cgroup/cpu\,cpuacct/system.slice/openresty.service/cpu.shares
