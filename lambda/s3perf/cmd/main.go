ec691d5c78681bb9338d30f65530e16d6f7c168c
Ariel Szekely <arielck@mit.edu>
2024-08-29 06:29:10 -0400

    x

--- lambda/s3perf/cmd/main.go
+++ lambda/s3perf/cmd/main.go
@@ -23,6 +23,7 @@ const (
 )
 
 type Event struct {
+	Type    string `json:"type"`
 	ObjPath string `json:"obj_path"`
 }
 
@@ -42,57 +43,73 @@ func NewResult(tpt string, err error) *Result {
 	}
 }
 
-func s3perf(s3cli *s3.S3, key string) *Result {
+func s3perf(s3cli *s3.S3, typ, key string) *Result {
 	bucket := "9ps3"
 	params := &s3.CreateMultipartUploadInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	}
-	result, err := s3cli.CreateMultipartUpload(params)
-	if err != nil {
-		return NewResult("", fmt.Errorf("Error CreateMultipartUpload: %v", err))
-	}
-	uploadID := *result.UploadId
-
-	b := make([]byte, BUFSZ)
-	for i := range b {
-		b[i] = 'g'
-	}
-	nbyte := 0
 	start := time.Now()
-	completedParts := []*s3.CompletedPart{}
-	for partNumber := int64(1); time.Since(start) < DUR; partNumber++ {
-		buf := filebuffer.New(b)
-		buf.Seek(0, io.SeekStart)
-		uploadParams := &s3.UploadPartInput{
-			Bucket:     aws.String(bucket),
-			Key:        aws.String(key),
-			UploadId:   aws.String(uploadID),
-			Body:       buf,
-			PartNumber: aws.Int64(partNumber),
-		}
-		result, err := s3cli.UploadPart(uploadParams)
+	nbyte := 0
+	if typ == "write" {
+		result, err := s3cli.CreateMultipartUpload(params)
 		if err != nil {
-			return NewResult("", fmt.Errorf("Error UploadPart: %v", err))
+			return NewResult("", fmt.Errorf("Error CreateMultipartUpload: %v", err))
 		}
-		completedParts = append(completedParts, &s3.CompletedPart{
-			ETag:       result.ETag,
-			PartNumber: aws.Int64(partNumber),
-		})
-		nbyte += len(b)
-	}
+		uploadID := *result.UploadId
 
-	completeParams := &s3.CompleteMultipartUploadInput{
-		Bucket:   aws.String(bucket),
-		Key:      aws.String(key),
-		UploadId: aws.String(uploadID),
-		MultipartUpload: &s3.CompletedMultipartUpload{
-			Parts: completedParts,
-		},
-	}
-	_, err = s3cli.CompleteMultipartUpload(completeParams)
-	if err != nil {
-		return NewResult("", fmt.Errorf("Error CompleteMultipartUpload: %v", err))
+		b := make([]byte, BUFSZ)
+		for i := range b {
+			b[i] = 'g'
+		}
+		completedParts := []*s3.CompletedPart{}
+		for partNumber := int64(1); time.Since(start) < DUR; partNumber++ {
+			buf := filebuffer.New(b)
+			buf.Seek(0, io.SeekStart)
+			uploadParams := &s3.UploadPartInput{
+				Bucket:     aws.String(bucket),
+				Key:        aws.String(key),
+				UploadId:   aws.String(uploadID),
+				Body:       buf,
+				PartNumber: aws.Int64(partNumber),
+			}
+			result, err := s3cli.UploadPart(uploadParams)
+			if err != nil {
+				return NewResult("", fmt.Errorf("Error UploadPart: %v", err))
+			}
+			completedParts = append(completedParts, &s3.CompletedPart{
+				ETag:       result.ETag,
+				PartNumber: aws.Int64(partNumber),
+			})
+			nbyte += len(b)
+		}
+
+		completeParams := &s3.CompleteMultipartUploadInput{
+			Bucket:   aws.String(bucket),
+			Key:      aws.String(key),
+			UploadId: aws.String(uploadID),
+			MultipartUpload: &s3.CompletedMultipartUpload{
+				Parts: completedParts,
+			},
+		}
+		_, err = s3cli.CompleteMultipartUpload(completeParams)
+		if err != nil {
+			return NewResult("", fmt.Errorf("Error CompleteMultipartUpload: %v", err))
+		}
+	} else if typ == "read" {
+		for time.Since(start) < DUR {
+			params := &s3.GetObjectInput{
+				Bucket: aws.String(bucket),
+				Key:    aws.String(key),
+				Range:  aws.String(fmt.Sprintf("bytes=%d-%d", 0, BUFSZ)),
+			}
+			_, err := s3cli.GetObject(params)
+			if err != nil {
+				return NewResult("", fmt.Errorf("Error GetObject: %v", err))
+			}
+			nbyte += BUFSZ
+		}
+
 	}
 	mb := float64(nbyte) / float64(MB)
 	tpt := mb / time.Since(start).Seconds()
@@ -106,12 +123,14 @@ func HandleRequest(ctx context.Context, event *Event) (*string, error) {
 		return nil, fmt.Errorf("received nil event")
 	}
 
-	sess, err := session.NewSession()
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
 	if err != nil {
 		return nil, err
 	}
 	s3cli := s3.New(sess)
-	res := s3perf(s3cli, event.ObjPath)
+	res := s3perf(s3cli, event.Type, event.ObjPath)
 	b, err := json.Marshal(res)
 	if err != nil {
 		return nil, fmt.Errorf("Error marshal json: %v", err)
@@ -125,8 +144,15 @@ func main() {
 	if os.Getenv("LOCAL_DEV") == "" {
 		lambda.Start(HandleRequest)
 	} else {
-		HandleRequest(context.TODO(), &Event{
+		res, err := HandleRequest(context.TODO(), &Event{
+			Type:    "write",
 			ObjPath: "s3perf-test-obj",
 		})
+		log.Printf("Write Res: %v\nErr:%v", *res, err)
+		res, err = HandleRequest(context.TODO(), &Event{
+			Type:    "read",
+			ObjPath: "s3perf-test-obj",
+		})
+		log.Printf("Read Res: %v\nErr:%v", *res, err)
 	}
 }
--- lambda/s3perf/scripts/invoke-fn.sh
+++ lambda/s3perf/scripts/invoke-fn.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage: $0 --obj-path OBJ_PATH" 1>&2
+  echo "Usage: $0 --obj-path OBJ_PATH --type TYPE" 1>&2
 }
 
 OBJ_PATH=""
+TYPE=""
 while [[ $# -gt 0 ]]; do
   key="$1"
   case $key in
   --obj-path)
     shift
     OBJ_PATH=$1
+    shift
+    ;;
+  --type)
+    shift
+    TYPE=$1
     shift
     ;;
   -help)
@@ -25,12 +31,12 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-if [ -z "$OBJ_PATH" ] || [ $# -gt 0 ]; then
+if [ -z "$OBJ_PATH" ] || [ -z "$TYPE" ] || [ $# -gt 0 ]; then
     usage
     exit 1
 fi
 
-b64=$(echo "{\"obj_path\": \"$OBJ_PATH\"}" | base64)
+b64=$(echo "{\"obj_path\": \"$OBJ_PATH\", \"type\": \"$TYPE\"}" | base64)
 aws lambda invoke --function-name s3perf response.json --payload $b64
 cat response.json
 rm -f response.json
