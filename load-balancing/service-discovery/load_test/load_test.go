eeab175a0967c76fcbd72f58233709eaed883207
Ariel Szekely <arielck@mit.edu>
2025-02-10 08:59:11 -0500

    Do some deletes as well

--- load-balancing/service-discovery/load_test/load_test.go
+++ load-balancing/service-discovery/load_test/load_test.go
@@ -2,6 +2,7 @@ package load_test
 
 import (
 	"flag"
+	"fmt"
 	"math/rand"
 	"strconv"
 	"sync"
@@ -16,7 +17,13 @@ import (
 	db "sigmaos/debug"
 )
 
+const (
+	PORT_OFFSET = 10000
+	SVC_NAME    = "mysvc"
+)
+
 var nClnt int
+var minEntries int
 var writeRPS int
 var readRPS int
 var dur time.Duration
@@ -24,10 +31,36 @@ var consulAddr string
 
 func init() {
 	flag.IntVar(&nClnt, "n_clnt", 1, "Number of clients")
+	flag.IntVar(&minEntries, "min_entries", 2, "Minimum number of entries in the service registry at any one time")
 	flag.DurationVar(&dur, "dur", 10*time.Second, "Duration")
 	flag.StringVar(&consulAddr, "consul_addr", "127.0.0.1:8500", "Consul server addr")
 	flag.IntVar(&writeRPS, "write_rps", 1, "Write requests per second")
 	flag.IntVar(&readRPS, "read_rps", 1, "Read requests per second")
+}
+
+func populateRegistry(t *testing.T, c *consul.Client) error {
+	db.DPrintf(db.TEST, "Populating service registry with %v entries", minEntries)
+	for i := 0; i < minEntries; i++ {
+		reg := &consul.AgentServiceRegistration{
+			ID:      "min-svc-replica-" + strconv.Itoa(i),
+			Name:    SVC_NAME,
+			Port:    i,
+			Address: "127.0.0.1",
+		}
+		if err := c.Agent().ServiceRegister(reg); err != nil {
+			return err
+		}
+	}
+	// Check that right number of services was registered
+	svcs, err := c.Agent().ServicesWithFilter("Service==" + SVC_NAME)
+	if !assert.Nil(t, err, "Err get first: %v", err) {
+		return err
+	}
+	if !assert.Equal(t, minEntries, len(svcs), "Wrong num entries") {
+		return fmt.Errorf("Wrong num entries %v != %v", minEntries, len(svcs))
+	}
+	db.DPrintf(db.TEST, "Done populating service registry with %v entries", minEntries)
+	return nil
 }
 
 func TestCompile(t *testing.T) {
@@ -45,13 +78,14 @@ func TestRegisterOnly(t *testing.T) {
 	db.DPrintf(db.TEST, "Created client")
 
 	var idx atomic.Int32
+	idx.Add(2)
 	writeLG := loadgen.NewLoadGenerator(dur, writeRPS, func(r *rand.Rand) (time.Duration, bool) {
 		i := int(idx.Add(1))
 		id := "svc-replica-" + strconv.Itoa(i)
 		reg := &consul.AgentServiceRegistration{
 			ID:      id,
-			Name:    "svc",
-			Port:    i,
+			Name:    SVC_NAME,
+			Port:    PORT_OFFSET + i,
 			Address: "127.0.0.1",
 		}
 		err := c.Agent().ServiceRegister(reg)
@@ -66,7 +100,49 @@ func TestRegisterOnly(t *testing.T) {
 	db.DPrintf(db.TEST, "Done")
 }
 
-func TestRegisterAndGet(t *testing.T) {
+func TestRegisterDeregisterOnly(t *testing.T) {
+	cfg := consul.DefaultConfig()
+	cfg.Address = consulAddr
+
+	c, err := consul.NewClient(cfg)
+	if !assert.Nil(t, err, "Err consul NewClient: %v", err) {
+		return
+	}
+	db.DPrintf(db.TEST, "Created client")
+
+	if err := populateRegistry(t, c); !assert.Nil(t, err, "Err register first: %v", err) {
+		return
+	}
+
+	var idx atomic.Int32
+	idx.Add(1)
+	writeLG := loadgen.NewLoadGenerator(dur, writeRPS, func(r *rand.Rand) (time.Duration, bool) {
+		i := int(idx.Add(1))
+		id := "svc-replica-" + strconv.Itoa(i/2)
+		if i%2 == 0 {
+			reg := &consul.AgentServiceRegistration{
+				ID:      id,
+				Name:    SVC_NAME,
+				Port:    PORT_OFFSET + i,
+				Address: "127.0.0.1",
+			}
+			err := c.Agent().ServiceRegister(reg)
+			assert.Nil(t, err, "Err register: %v", err)
+		} else {
+			err := c.Agent().ServiceDeregister(id)
+			assert.Nil(t, err, "Err deregister: %v", err)
+		}
+		return 0, false
+	})
+	db.DPrintf(db.TEST, "Calibrating write load generator")
+	writeLG.Calibrate()
+	db.DPrintf(db.TEST, "Done calibrating read load generator")
+	writeLG.Run()
+	writeLG.Stats()
+	db.DPrintf(db.TEST, "Done")
+}
+
+func TestRegisterDeregisterGet(t *testing.T) {
 	cfg := consul.DefaultConfig()
 	cfg.Address = consulAddr
 
@@ -77,26 +153,9 @@ func TestRegisterAndGet(t *testing.T) {
 
 	db.DPrintf(db.TEST, "Created client")
 
-	reg := &consul.AgentServiceRegistration{
-		ID:      "svc-replica-0",
-		Name:    "svc",
-		Port:    0,
-		Address: "127.0.0.1",
-	}
-	err = c.Agent().ServiceRegister(reg)
-	if !assert.Nil(t, err, "Err register first: %v", err) {
+	if err := populateRegistry(t, c); !assert.Nil(t, err, "Err register first: %v", err) {
 		return
 	}
-	db.DPrintf(db.TEST, "Registered a service endpoint")
-
-	svcs, err := c.Agent().ServicesWithFilter("Service==svc")
-	if !assert.Nil(t, err, "Err get first: %v", err) {
-		return
-	}
-	if !assert.Equal(t, 1, len(svcs), "Wrong num services") {
-		return
-	}
-	db.DPrintf(db.TEST, "Read a service endpoint")
 
 	var wg sync.WaitGroup
 
@@ -104,19 +163,24 @@ func TestRegisterAndGet(t *testing.T) {
 	idx.Add(1)
 	writeLG := loadgen.NewLoadGenerator(dur, writeRPS, func(r *rand.Rand) (time.Duration, bool) {
 		i := int(idx.Add(1))
-		id := "svc-replica-" + strconv.Itoa(i)
-		reg := &consul.AgentServiceRegistration{
-			ID:      id,
-			Name:    "svc",
-			Port:    i,
-			Address: "127.0.0.1",
+		id := "svc-replica-" + strconv.Itoa(i/2)
+		if i%2 == 0 {
+			reg := &consul.AgentServiceRegistration{
+				ID:      id,
+				Name:    SVC_NAME,
+				Port:    PORT_OFFSET + i,
+				Address: "127.0.0.1",
+			}
+			err := c.Agent().ServiceRegister(reg)
+			assert.Nil(t, err, "Err register: %v", err)
+		} else {
+			err := c.Agent().ServiceDeregister(id)
+			assert.Nil(t, err, "Err deregister: %v", err)
 		}
-		err := c.Agent().ServiceRegister(reg)
-		assert.Nil(t, err, "Err register: %v", err)
 		return 0, false
 	})
 	readLG := loadgen.NewLoadGenerator(dur, readRPS, func(r *rand.Rand) (time.Duration, bool) {
-		svcs, err := c.Agent().ServicesWithFilter("Service==svc")
+		svcs, err := c.Agent().ServicesWithFilter("Service==" + SVC_NAME)
 		assert.Nil(t, err, "Err get: %v", err)
 		assert.True(t, len(svcs) > 1, "No services returned")
 		return 0, false
