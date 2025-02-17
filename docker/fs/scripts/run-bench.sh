eddd15edeb65d64ffee905e7834a5beffb1d35d4
Ariel Szekely <arielck@mit.edu>
2025-02-17 16:26:04 -0500

    Add bench runner script

--- docker/fs/scripts/run-bench.sh
+++ docker/fs/scripts/run-bench.sh
@@ -10,7 +10,6 @@ docker run -it \
   --network host \
   --mount type=bind,src=$(pwd),dst=/app \
   arielszekely/docker-fs-bench \
-  go test -v fs_test.go
+  go test -v fs_test.go --n_dummy_files 1000
 
-
-echo "done starting ctr"
+echo "done running bench"
